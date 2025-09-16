4f48481599826d335713db9f4957d175e3bb9db6
Harshvardhan Dixit <kacked07@gmail.com>
2025-09-16 22:11:06 +0530

    food schema written

--- backend/models/food.model.js
+++ backend/models/food.model.js
@@ -1,1 +1,29 @@
 import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required : true,
+        trim : true
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    price : {
+        type: Number,
+        required : true,
+
+    },
+    category : {
+        type: String,
+        
+        enum : ["INDIAN", "CHINESE", "MEXICAN","JAPANESE" ],
+        default : "INDIAN"
+    },
+    image : {
+        type : String,
+        
+    }
+    }
+,{timestamps: true})
