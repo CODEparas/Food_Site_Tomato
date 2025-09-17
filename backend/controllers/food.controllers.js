a62a2b16db129f65534ddff3eb1b28d4c0d868ca
Harshvardhan Dixit <kacked07@gmail.com>
2025-09-18 01:06:42 +0530

    Added food item creation logic

---
+++ backend/controllers/food.controllers.js
@@ -0,0 +1,48 @@
+import Food from "../models/food.model.js";
+import mongoose from "mongoose";
+
+
+export const addFood = async (req ,res , next)=>{
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const {name,description,price} = req.body;
+    if(!name || !description || !price){
+        const error = new Error("Enter all descriptions");
+        error.statusCode = 400;
+        throw error;
+    }
+
+    const fooditem = await Food.create([{
+        name,
+        description,
+        price
+
+    }], {session});
+
+     const food = fooditem[0];
+
+
+    await session.commitTransaction();
+
+    res.status(201).json({
+        success: true,
+        message : "Food item added",
+        data : {
+            food,
+        }
+    })
+
+
+        
+    } catch (error) {
+        await session.abortTransaction();
+        next(error);
+        
+    }
+
+    finally{
+        await session.endSession();
+    }
+
+}
