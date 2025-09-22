00222a9353a4927c54da4c42dc68c680c3467d70
Harshvardhan Dixit <kacked07@gmail.com>
2025-09-22 21:30:18 +0530

    changed routes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,18 +1,19 @@
 import { Router } from "express";   
 import { getAllorder, UserOrder,createOrder, updateOrderStatusById } from "../controllers/order.controller";  
 import { get } from "mongoose";
+import authorize from "../middlewares/auth.middlewares";
 
 
 
 const orderRouter = Router();
 
 // Define order-related routes here
-orderRouter.get('/list', getAllorder);
+orderRouter.get('/list',authorize, getAllorder);
 
-orderRouter.post('/create', createOrder);
+orderRouter.post('/create',authorize, createOrder);
 
-orderRouter.get('/:userId', UserOrder);
+orderRouter.get('/:userId',authorize, UserOrder);
 
-orderRouter.put('/:orderId', updateOrderStatusById);
+orderRouter.put('/:orderId',authorize, updateOrderStatusById);
 
 export default orderRouter;
